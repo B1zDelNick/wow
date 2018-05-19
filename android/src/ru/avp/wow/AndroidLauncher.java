00805954704398a7bb17a553b34550c99ecb1512
avasilevich <andrey.vasilevich@pressindex.ru>
2018-05-19 17:11:03 +0700

    Initial commit

---
+++ android/src/ru/avp/wow/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.avp.wow;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.avp.wow.WowGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WowGame(), config);
+	}
+}
